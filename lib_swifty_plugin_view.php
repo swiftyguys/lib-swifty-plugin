38e65a5be64fd00de8109c48f59ca8fc8eba4960
martijn.van.der.kooij <martijn@swiftyguys.com>
2014-10-16 15:42:39 +0200

    Only show screen size icons in swifty site

--- lib_swifty_plugin_view.php
+++ lib_swifty_plugin_view.php
@@ -22,11 +22,13 @@ class LibSwiftyPluginView
     public static $required_plugin_active_ninja_forms = false;
     public static $required_plugin_active_shortcodes_ultimate = false;
     public static $required_plugin_active_wordpress_canvas_gallery = false;
+    public static $required_plugin_active_swifty_site = false;
 
     public function action_plugins_loaded()
     {
         self::$required_plugin_active_ninja_forms = defined( 'NF_PLUGIN_VERSION' );
         self::$required_plugin_active_shortcodes_ultimate = defined( 'SU_PLUGIN_FILE' );
         self::$required_plugin_active_wordpress_canvas_gallery = defined( 'WC_GALLERY_VERSION' );
+        self::$required_plugin_active_swifty_site = defined( 'SWIFTY_SITE_PLUGIN_URL' );
     }
 }
