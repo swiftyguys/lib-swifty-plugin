1f3c3dbe1111056746484200a739f1eb0656c0e0
martijn.van.der.kooij <martijn@swiftyguys.com>
2015-03-19 14:07:40 +0100

    ExzXiK4z copied css styles to swifty lib

--- lib_swifty_plugin.php
+++ lib_swifty_plugin.php
@@ -74,27 +74,85 @@ class LibSwiftyPlugin extends LibSwiftyPluginView
         $tab = isset( $_GET[ 'tab' ] ) && array_key_exists( $_GET[ 'tab' ], $settings_tabs ) ? $_GET[ 'tab' ] : 'scc_options';
 
         ?>
+
+        <script type="text/javascript">
+
+            jQuery( function( $ ) {
+                <?php if( $this->is_ss_mode() ) : ?>
+                $( '#wpadminbar' ).hide();
+                $( '#adminmenuback' ).hide();
+                $( '#adminmenuwrap' ).hide();
+                $( '#wpcontent' ).css( 'margin-left', '0px' );
+                //$('.wp-toolbar').css('padding-top', '0px');
+                $( '.updated' ).hide();
+                $( '.error' ).hide();
+                <?php endif ?>
+
+                $( document ).on( 'click', '.swifty_back_button', function( /*ev*/ ) {
+                    // in worst case fallback to main page
+                    var backLocation = window.location.protocol + '//' +
+                        window.location.hostname + ':' +
+                        window.location.port + '/';
+
+                    if ( typeof Storage !== 'undefined' ) {
+                        if ( sessionStorage.back_location ) {
+                            backLocation = sessionStorage.back_location;
+                        }
+                    }
+
+                    window.location = backLocation;
+                });
+
+            } );
+
+
+        </script>
+
+
+
         <div class='wrap'>
-            <h2><?php echo $admin_page_title; ?></h2>
 
-            <?php settings_errors( 'api-manager' ); ?>
-
-            <h2 class="nav-tab-wrapper">
-                <?php
-                foreach( $settings_tabs as $tab_page => $tab_info ) {
-                    $active_tab = $tab == $tab_page ? 'nav-tab-active' : '';
-                    echo '<a class="nav-tab ' . $active_tab . '" href="?page=' . $admin_page . '&tab=' . $tab_page . '">' . $tab_info[ 'title' ] . '</a>';
-                }
-                ?>
-            </h2>
-
-            <form action='options.php' method='post'>
-                <div class="main">
-                    <?php
-                    call_user_func( $settings_tabs[ $tab ][ 'method' ] );
-                    ?>
+            <?php if( $this->is_ss_mode() ) : ?>
+                <div class="swifty_panel_title_container">
+                    <div class="swifty_panel_title_strike"></div>
+                    <div class="swifty_panel_title_pos">
+                        <div class="swifty_title">
+                            <?php endif ?>
+                            <h2><?php echo $admin_page_title; ?></h2>
+                            <?php if( $this->is_ss_mode() ) : ?>
+                        </div>
+                    </div>
                 </div>
-            </form>
+                <br><br>
+            <?php endif ?>
+
+            <?php if( $this->is_ss_mode() ) : ?>
+                <div class="swifty_back_button swifty_button">
+                    <i class="fa fa-caret-left"></i>
+                </div>
+            <?php endif ?>
+
+            <div class="swifty-wrapper<?php echo $this->is_ss_mode() ? ' swifty_content_right' : ''; ?>">
+
+                <?php settings_errors( 'api-manager' ); ?>
+
+                <h2 class="nav-tab-wrapper">
+                    <?php
+                    foreach( $settings_tabs as $tab_page => $tab_info ) {
+                        $active_tab = $tab == $tab_page ? 'nav-tab-active' : '';
+                        echo '<a class="nav-tab ' . $active_tab . '" href="?page=' . $admin_page . '&tab=' . $tab_page . '">' . $tab_info[ 'title' ] . '</a>';
+                    }
+                    ?>
+                </h2>
+
+                <form action='options.php' method='post'>
+                    <div class="main">
+                        <?php
+                        call_user_func( $settings_tabs[ $tab ][ 'method' ] );
+                        ?>
+                    </div>
+                </form>
+            </div>
         </div>
     <?php
     }
@@ -124,3 +182,46 @@ class LibSwiftyPlugin extends LibSwiftyPluginView
     }
 
 }
+
+function admin_enqueue_styles()
+{
+    if ( is_user_logged_in() ) {
+
+        global $swifty_css_admin_url;
+        global $swifty_css_admin_version;
+
+        wp_enqueue_style(
+            'swifty-admin.css',
+            $swifty_css_admin_url,
+            array(),
+            $swifty_css_admin_version,
+            'all'
+        );
+
+        wp_enqueue_style( 'swifty-font-awesome', '//netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css' );
+    }
+}
+
+$css_admin_version = (int)'/*@echo CSS_ADMIN_REL_TAG*/';
+
+global $swifty_css_admin_version;
+global $swifty_css_admin_url;
+global $scc_buildUse;
+
+if( !isset( $swifty_css_admin_version ) || ( $swifty_css_admin_version < $css_admin_version ) ) {
+    $swifty_css_admin_version = $css_admin_version;
+
+    // we need to work around the plugin dir link we use in our development systems
+    $plugin_dir      = dirname( dirname( dirname( __FILE__ ) ) );
+    // get plugin name
+    $plugin_basename = basename( $plugin_dir );
+    $plugin_dir_url  = trailingslashit( plugins_url( rawurlencode( $plugin_basename ) ) );
+
+    if( $scc_buildUse == 'build' ) {
+        $swifty_css_admin_url = $plugin_dir_url . 'css/swifty-admin.css';
+    } else {
+        $swifty_css_admin_url = $plugin_dir_url . 'lib/swifty_plugin/css/swifty-admin.css';
+    }
+}
+
+add_action( 'admin_enqueue_scripts', 'admin_enqueue_styles' );
