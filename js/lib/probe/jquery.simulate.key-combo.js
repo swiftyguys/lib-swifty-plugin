291851364b68cb58522a95d5687acc31cf7ac88d
Robert Heessels <robert@heessels.com>
2015-12-07 11:28:37 +0100

    Working on tests.

--- js/lib/probe/jquery.simulate.key-combo.js
+++ js/lib/probe/jquery.simulate.key-combo.js
@@ -13,7 +13,6 @@
  * For details about key events, key codes, char codes etc. see http://unixpapa.com/js/key.html
  */
 
-console.log( 'aaa' );
 ;(function($,undefined) {
 	"use strict";
 
@@ -143,7 +142,6 @@ console.log( 'aaa' );
 						options.eventProps.which = keyCode;
 						options.eventProps.charCode = 0;
 						$target.simulate("keydown", options.eventProps);
-
 					}
 					else {
 						// "Normal" key
