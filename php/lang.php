632223329d4df6796c647fd68695877acbb03eee
martijn.van.der.kooij <martijn@swiftyguys.com>
2016-04-11 10:57:45 +0200

    utIyRdMy moved area names to lib for translation in both scc and ssd

---
+++ php/lang.php
@@ -0,0 +1,14 @@
+<?php
+
+// this files contains texts that needs to be translated in several locations and plugins
+
+
+// For translations:
+__( 'page', 'swifty-content-creator' );
+__( 'topbar', 'swifty-content-creator' );
+__( 'header', 'swifty-content-creator' );
+__( 'navbar', 'swifty-content-creator' );
+__( 'sidebar', 'swifty-content-creator' );
+__( 'extrasidebar', 'swifty-content-creator' );
+__( 'footer', 'swifty-content-creator' );
+__( 'bottombar', 'swifty-content-creator' );
