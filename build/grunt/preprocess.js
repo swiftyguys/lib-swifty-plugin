5999025442a560d3c4c71554e3eb2afea061dee8
Sysadmin on Cactus <sysadmin@cactus.alphamegahosting.com>
2016-04-28 09:49:00 +0200

    Building scc1.4.6

--- build/grunt/preprocess.js
+++ build/grunt/preprocess.js
@@ -3,7 +3,7 @@ module.exports = function( grunt/*, options*/ ) {
         dist: {
             //src: '<%= grunt.getDestPathPlugin() %>' + grunt.myCfg.plugin_code + '.php',
             //dest: '<%= grunt.getDestPathPlugin() %>' + grunt.myCfg.plugin_code + '.php'
-            src : [ '<%= grunt.getDestPathPlugin() %>**/*.php' ],
+            src : [ '<%= grunt.getDestPathPlugin() %>**/*.php', '<%= grunt.getDestPathPlugin() %>**/swifty-font.css' ],
             options: {
               inline : true
             }
